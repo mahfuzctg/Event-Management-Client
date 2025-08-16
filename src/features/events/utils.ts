f0a42c81e1111e5669ea504724cd1fe3d36e08b3
Mahfuz <aamahfuz.pro@gmail.com>
2025-08-16 14:37:39 +0600

    featers/events: added utils

---
+++ src/features/events/utils.ts
@@ -0,0 +1,16 @@
+import { IEvent, EventStatus } from "@/types/event";
+import dayjs from "dayjs";
+
+export const getEventStatus = (event: IEvent): EventStatus => {
+  const now = dayjs();
+  const start = dayjs(event.startDate);
+  const end = dayjs(event.endDate);
+
+  if (now.isBefore(start)) return "upcoming";
+  if (now.isAfter(end)) return "past";
+  return "ongoing";
+};
+
+export const sortEventsChronologically = (events: IEvent[]): IEvent[] => {
+  return events.sort((a, b) => dayjs(a.startDate).valueOf() - dayjs(b.startDate).valueOf());
+};
